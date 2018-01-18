bcb2c34d6eadf1f8a5e5b1ab9b86b82ab3be619a
DefNeoRoman <d8905611060@yandex.ru>
2018-01-18 11:41:26 +0300

    init

---
+++ src/main/java/Testing.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.stream.Stream;
+
+public class Testing {
+    public void parseCtbFile() {
+       File file = new File("D:\\mySandbox\\testGit\\testGit\\src\\resources\\test.txt");
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            Stream<String> lines = new BufferedReader(new InputStreamReader(fileInputStream)).lines();
+            lines.forEach(System.out::println);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+
+        Testing testing = new Testing();
+        testing.parseCtbFile();
+    }
+}
