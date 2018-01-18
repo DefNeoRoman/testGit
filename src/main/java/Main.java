c9b9f962d357d50b8f66f19e9f8a50d1210a3473
DefNeoRoman <d8905611060@yandex.ru>
2018-01-18 10:13:13 +0300

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        String  filename = "34.ctb";
+
+        if(filename.endsWith(".ctb")){
+            System.out.println("yes");
+        }
     }
     public void firstMethod(){
         
