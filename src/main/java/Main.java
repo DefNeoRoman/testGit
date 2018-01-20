74a7fc54e09af14de52ba486118e6ee79cd01f99
DefNeoRoman <d89056411060@yandex.ru>
2018-01-20 18:33:09 +0300

    С2_Branch2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String  filename = "34.ctb";
         System.out.println("Сообщение во втором коммите");
+        System.out.println("Сообщение в третьем коммите в branch2");
     }
     public void firstMethod(){
 
