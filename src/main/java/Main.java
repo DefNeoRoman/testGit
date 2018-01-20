d0c97aa193dfe347f362bf7c1bb1aaf73c3c7519
DefNeoRoman <d89056411060@yandex.ru>
2018-01-20 18:31:40 +0300

    С1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         String  filename = "34.ctb";
-
-        if(filename.endsWith(".ctb")){
-            System.out.println("yes");
-        }
+        System.out.println("Сообщение во втором коммите");
     }
     public void firstMethod(){
-        
+
     }
 }
