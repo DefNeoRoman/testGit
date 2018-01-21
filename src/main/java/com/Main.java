bc5bfabb2c643c623958339d77ea8e83e126765c
rgaponov <d89056411060@yandex.ru>
2018-01-21 16:51:18 +0300

    in master

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("сообщение в мастер");
         System.out.println("second message в мастер");
-
+        System.out.println("third");
     }
     public void firstMethod(){
 
