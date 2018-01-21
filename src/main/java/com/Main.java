7ff2f4f5c4c7e1e3db038d7e61c437112c342182
rgaponov <d89056411060@yandex.ru>
2018-01-21 15:14:40 +0300

    add second message in br4

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("сообщение в br4");
+        System.out.println("second message в br4");
 
     }
     public void firstMethod(){
