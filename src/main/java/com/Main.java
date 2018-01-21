4296aed697cd5075a4b053bd9e4677d3d13f7bdb
rgaponov <d89056411060@yandex.ru>
2018-01-21 14:53:50 +0300

    commit after second merge

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("второе Сообщение в ветке aftermerge");
         System.out.println("третье Сообщение  ветке мастер после слияния с br3 в ветке br3");
         System.out.println("сообщение после второго слияния");
+        System.out.println("второе сообщение после второго слияния");
 
 
     }
