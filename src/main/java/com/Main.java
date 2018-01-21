1d96f4ed54c95eeac033cb05023478c22bd63258
rgaponov <d89056411060@yandex.ru>
2018-01-21 16:45:22 +0300

    in br10 message

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,7 +3,7 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("сообщение в мастер");
+        System.out.println("сообщение в br10");
 
     }
     public void firstMethod(){
