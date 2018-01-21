6151906fa7743f50388735310790c8a80656542c
DefNeoRoman <d89056411060@yandex.ru>
2018-01-21 11:26:44 +0300

    ветка мастер

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,8 +1,10 @@
+package com;
+
 public class Main {
 
     public static void main(String[] args) {
         String  filename = "34.ctb";
-        System.out.println("Сообщение во втором коммите");
+        System.out.println("Сообщение в ветке мастер");
     }
     public void firstMethod(){
 
