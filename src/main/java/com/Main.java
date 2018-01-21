8b863e702555354a147f54745adaf00c1cecf499
rgaponov <d89056411060@yandex.ru>
2018-01-21 14:45:58 +0300

    second message in aftermerge

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String  filename = "34.ctb";
         System.out.println("Сообщение в ветке aftermerge");
+        System.out.println("второе Сообщение в ветке aftermerge");
 
 
     }
