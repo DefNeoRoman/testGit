5270ca78629e0e63d76f330a952f76f6737e1831
DefNeoRoman <d89056411060@yandex.ru>
2018-01-21 11:41:53 +0300

    change in branch2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
         String  filename = "34.ctb";
         System.out.println("Сообщение во втором коммите");
         System.out.println("Сообщение в третьем коммите в branch2");
+        System.out.println("еще одно сообщение в branch2");
     }
     public void firstMethod(){
 
