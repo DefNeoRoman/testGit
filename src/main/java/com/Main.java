07af080f7dac180e76949c0751c1cff2306e4e36
rgaponov <d89056411060@yandex.ru>
2018-01-21 15:13:51 +0300

    add message in br4

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,13 +3,7 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-        String  filename = "34.ctb";
-        System.out.println("Сообщение в ветке мастер");
-        System.out.println("второе сообщение в веткемастер");
-        System.out.println("третье сообщение в ветке мастер");
-        System.out.println("четвертое сообщение в ветке мастер");
-        System.out.println("пятое сообщение в ветке мастер");
-        System.out.println("шестое сообщение в ветке мастер");
+        System.out.println("сообщение в br4");
 
     }
     public void firstMethod(){
