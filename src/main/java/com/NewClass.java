de5ee42595441278d9969791aef6220ed4e64797
rgaponov <d89056411060@yandex.ru>
2018-03-04 11:02:34 +0300

    sec commmit in B2

--- src/main/java/com/NewClass.java
+++ src/main/java/com/NewClass.java
@@ -3,6 +3,7 @@ package com;
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("message in home B2");
+        System.out.println("secong commit in home B2");
     }
 
 }
