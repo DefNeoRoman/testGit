ba10d747d45a32ae0b8b18d7e66ed76a7a42bfe9
rgaponov <d89056411060@yandex.ru>
2018-03-04 11:05:38 +0300

    master commmit in B2

--- src/main/java/com/NewClass.java
+++ src/main/java/com/NewClass.java
@@ -4,6 +4,7 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("message in home B2");
         System.out.println("secong commit in home B2");
+        System.out.println("message  in master");
     }
 
 }
